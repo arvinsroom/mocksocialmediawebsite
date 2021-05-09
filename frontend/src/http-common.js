7ed48d8611a9b55be12f618e3ac433992c9eb1e7
gs3bopar <gurkaranboparai1120@gmail.com>
2021-05-09 19:58:58 -0400

    fix nginx

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const environment = process.env.NODE_ENV;
 
 export default axios.create({
-  baseURL: environment === 'development' ? 'http://localhost:8081/api' : 'https://studysocial.media:8081/api',
+  baseURL: environment === 'development' ? 'http://localhost:8081/api' : 'http://studysocial.media:8081/api',
   // baseURL: `http://${IP_ADDRESS}:8081/api`,
   headers: {
     "Content-type": "application/json"
